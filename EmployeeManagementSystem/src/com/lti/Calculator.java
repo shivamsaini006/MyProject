0deb579fa2524866790f3e5c90d9373fe368368d
shiva <shiva@LAPTOP-9J56FB1D>
2020-08-06 09:47:41 +0530

    Difference Added

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -3,7 +3,10 @@ package com.lti;
 public class Calculator {
 
 	public static void add(int a, int b) {
-		System.out.println("Sum = "+(a+b));
+		System.out.println("Sum is = "+(a+b));
+	}
+	public static void diff(int a, int b) {
+		System.out.println("Difference is = "+(a-b));
 	}
 	public static void main(String[] args) {
 		add(10,30);
